8eee8cd760b87560d60816b2ca680b716bdb33c2
arthutepio <arthurodriguesr10@gmail.com>
2023-02-17 14:56:37 -0300

    feat: create class Mage e Necromancer that extends Archetypes

---
+++ src/Archetypes/Necromancer.ts
@@ -0,0 +1,23 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Necromancer extends Archetype {
+  private _energyType: EnergyType;
+  private static _createdArchetypeInstances = 0;
+
+  constructor(name: string) {
+    super(name);
+
+    this._energyType = 'mana';
+
+    Necromancer._createdArchetypeInstances += 1;
+  }
+
+  public static createdArchetypeInstances(): number {
+    return Necromancer._createdArchetypeInstances;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
