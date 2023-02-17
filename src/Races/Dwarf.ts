eb1185727c422249391a51150825291cbdfad9bd
arthutepio <arthurodriguesr10@gmail.com>
2023-02-17 08:57:18 -0300

    feat: create class Dwarf that extends class Race

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+export default class Dwarf extends Race {
+  private _maxLifePoints: number;
+  private static _createdInstancesCount = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+
+    this._maxLifePoints = 80;
+
+    Dwarf._createdInstancesCount += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return Dwarf._createdInstancesCount;
+  }
+}
